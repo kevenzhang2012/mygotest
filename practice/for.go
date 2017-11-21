9292eda862204fe89f0d27a195abf834dd60ba7a
张军涛 <zhangjuntao@zhangjuntao.local>
2017-11-21 09:09:07 +0800

    add for

--- practice/for.go
+++ practice/for.go
@@ -6,10 +6,28 @@ import (
 
 func main() {
 	i := 1
-	for i := 0; i < 3; i++ {
+	for i < 3 {
 
 		fmt.Println(i)
 		i = i + 1
 
 	}
+
+	for j := 7; j <= 10; j++ {
+		fmt.Println(j)
+
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for t := 0; t < 20; t++ {
+		if t%2 == 0 {
+		}
+
+		fmt.Println(t)
+	}
+
 }
